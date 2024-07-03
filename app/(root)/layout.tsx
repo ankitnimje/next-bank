1098f716e297e288b1e60d69b97fb2b4f03aab6a
ankit <ankit.nimje93@gmail.com>
2024-07-02 20:40:07 -0500

    add sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,40 @@
+import Image from "next/image";
+import Sidebar from "@/components/Sidebar";
+import MobileNavbar from "@/components/MobileNavbar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInUserMockData = {
+    $id: "123",
+    email: "el@pacino.com",
+    userId: "12345",
+    dwollaCustomerUrl: "some url",
+    dwollaCustomerId: "some id",
+    firstName: "AL",
+    lastName: "Pacino",
+    address1: "Broadway st",
+    city: "LA",
+    state: "CA",
+    postalCode: "66666",
+    dateOfBirth: "12/10/1299",
+    ssn: "123-12301",
+  };
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedInUserMockData} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNavbar user={loggedInUserMockData} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
