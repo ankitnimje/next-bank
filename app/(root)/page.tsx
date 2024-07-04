7c32c34a766ef56ac53493e2df1caef49dab46e5
ankit <ankit.nimje93@gmail.com>
2024-07-04 15:12:02 -0500

    add right side bar with basic details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,33 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-
-  const loggedIn = {
-    firstName: 'Ankit'
-  }
+  const loggedInUserMockData = {
+    $id: "123",
+    email: "el@pacino.com",
+    userId: "12345",
+    dwollaCustomerUrl: "some url",
+    dwollaCustomerId: "some id",
+    firstName: "AL",
+    lastName: "Pacino",
+    address1: "Broadway st",
+    city: "LA",
+    state: "CA",
+    postalCode: "66666",
+    dateOfBirth: "12/10/1299",
+    ssn: "123-12301",
+  };
 
   return (
-    <section className='home'>
+    <section className="home">
       <div className="home-content">
-        <header className='home-header'>
+        <header className="home-header">
           <HeaderBox
-            type='greeting'
+            type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedInUserMockData?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
@@ -25,9 +37,11 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedInUserMockData} transactions={[]} banks={[]} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
