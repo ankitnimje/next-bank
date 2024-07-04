3876f3868e8c47fbdd6b87300c068c063cd543e3
ankit <ankit.nimje93@gmail.com>
2024-07-04 16:31:09 -0500

    add auth pages template

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -6,9 +6,6 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
