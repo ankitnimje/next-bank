e48d724c0f681f1cdb81a8788daf4b36026c09d2
ankit <ankit.nimje93@gmail.com>
2024-07-08 20:13:22 -0400

    add sentry configuration

---
+++ sentry.server.config.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://88e3763f8e6094ce5f5b35bbb16c0961@o4507568822550528.ingest.us.sentry.io/4507568827793408",
+
+  integrations: [Sentry.replayIntegration()],
+
+  // Session replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+});
